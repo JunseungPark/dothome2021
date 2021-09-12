8c3c7b29bd425f6b2d8705312408bddc7ea4cf8a
JunseungPark <dpfwl8745@gmail.com>
2021-09-13 00:36:42 +0900

    21/09/13 작업

---
+++ site-php/login/login.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="../assets/css/fonts.css">
+    <link rel="stylesheet" href="../assets/css/var.css">
+    <link rel="stylesheet" href="../assets/css/reset.css">
+    <link rel="stylesheet" href="../assets/css/style.css">
+</head>
+<body>
+<div id="skip">
+        <a href="#contents">컨텐츠 바로가기</a>
+        <a href="#footer">풋터 바로가기</a>
+    </div>
+    <!-- //skip -->
+    <header id="header">
+        <h1><a href="#">php <em>class</a></em></h1>
+        <nav>
+            <h2 class="ir_so">메인 메뉴</h2>
+            <ul>
+                <li><a href="#">댓글</a></li>
+                <li><a href="#">회원가입</a></li>
+                <li><a href="#">로그인</a></li>
+                <li><a href="#">게시판</a></li>
+                <li><a href="#">블로그</a></li>
+            </ul>
+        </nav>
+        <div class="member">
+            <strong class="ir_so">회원 정보 영역</strong>
+            <a href="../login/login.php">로그인</a>
+            <a href="../login/join.php">회원가입</a>
+        </div>
+    </header>
+    <!--// header -->
+
+    <main id="contents">
+        <section id="mainCont">
+        <h2>회원 가입</h2>
+        <article class="content-article"></article>
+            <div class="join-form">
+                <h3>회원가입</h3>
+            </div>
+        </section>
+    </main>
+    <!--// contents -->
+
+    <footer id="footer">
+        footer
+    </footer>
+    <!--// footer -->
+</body>
+</html>
